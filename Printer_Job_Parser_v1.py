5f57f09b3f27259b88a7c535d7ecbb92d1ed1df4
Sunny-patel-2207 <49509859+Sunny-patel-2207@users.noreply.github.com>
2021-12-29 16:38:40 +0530

    Delete Printer_Job_Parser_v1.py

--- Printer_Job_Parser_v1.py
+++
@@ -1,5 +0,0 @@
-import tabula
-import json
-def lambda_handler(event, context):
-    print('Execution is start!!!!.....')
-    tabula.read_pdf('https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf', pages='all')
